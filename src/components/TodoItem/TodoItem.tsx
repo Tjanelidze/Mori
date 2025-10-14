ae15f0178e91139d2e2b472c96a8172ed5858a9e
t.janelidze <tedojanelidze7@gmail.com>
2025-10-14 22:15:40 +0400

    feat: added api for todos, added library for generating id, implemented pagination

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,19 +1,32 @@
+import type { Todos } from "../../App";
+
 interface TodoItemProps {
-  todo: string;
-  todoId: number;
-  onDelete: (todoId: number) => void;
+  todo: Todos;
+  todoId: string;
+  onDelete: (todoId: string) => void;
+  onEdit: (todoId: string) => void;
 }
 
-export const TodoItem = ({ todo, onDelete, todoId }: TodoItemProps) => {
+export const TodoItem = ({ todo, todoId, onDelete, onEdit }: TodoItemProps) => {
   return (
     <div className="flex justify-between p-2">
-      <p className="text-indigo-700">{todo}</p>
-      <button
-        className="cursor-pointer text-red-600"
-        onClick={() => onDelete(todoId)}
-      >
-        X
-      </button>
+      <p className="text-indigo-700">{todo.title}</p>
+
+      <div className="flex gap-7">
+        <button
+          className="cursor-pointer font-bold text-emerald-800"
+          onClick={() => onEdit(todoId)}
+        >
+          /
+        </button>
+
+        <button
+          className="cursor-pointer text-red-600"
+          onClick={() => onDelete(todoId)}
+        >
+          X
+        </button>
+      </div>
     </div>
   );
 };
