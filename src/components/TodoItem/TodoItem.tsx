6629ed8de1567c896e8a08252383e3ed841a12fc
t.janelidze <tedojanelidze7@gmail.com>
2025-10-17 14:18:54 +0400

    started implementing edit feature

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,13 +1,50 @@
+import { useState } from "react";
 import type { Todos } from "../../App";
 
 interface TodoItemProps {
   todo: Todos;
-  todoId: string;
+  selectedTodo?: Todos;
+  setEditTodo: React.Dispatch<React.SetStateAction<Todos | undefined>>;
   onDelete: (todoId: string) => void;
   onEdit: (todoId: string) => void;
 }
 
-export const TodoItem = ({ todo, todoId, onDelete, onEdit }: TodoItemProps) => {
+export const TodoItem = ({
+  todo,
+  onDelete,
+  onEdit,
+  selectedTodo,
+  setEditTodo,
+}: TodoItemProps) => {
+  const [isEdit, setIsEdit] = useState(false);
+
+  if (isEdit)
+    return (
+      <form
+        className="flex justify-between p-2"
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          setIsEdit(false);
+        }}
+      >
+        <input
+          type="text"
+          value={selectedTodo?.title}
+          onChange={(e) => {
+            const newTodo = {
+              ...selectedTodo,
+              title: e.target.value,
+            };
+
+            setEditTodo(newTodo);
+          }}
+        />
+
+        <button className="cursor-pointer text-red-600">X</button>
+      </form>
+    );
+
   return (
     <div className="flex justify-between p-2">
       <p className="text-indigo-700">{todo.title}</p>
@@ -15,14 +52,17 @@ export const TodoItem = ({ todo, todoId, onDelete, onEdit }: TodoItemProps) => {
       <div className="flex gap-7">
         <button
           className="cursor-pointer font-bold text-emerald-800"
-          onClick={() => onEdit(todoId)}
+          onClick={() => {
+            setIsEdit(true);
+            onEdit(todo.id);
+          }}
         >
           /
         </button>
 
         <button
           className="cursor-pointer text-red-600"
-          onClick={() => onDelete(todoId)}
+          onClick={() => onDelete(todo.id)}
         >
           X
         </button>
