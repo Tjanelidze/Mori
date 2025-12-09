f6bb452a29824028c1ca4ac5cf965d3510271598
t.janelidze <tedojanelidze7@gmail.com>
2025-12-09 14:22:31 +0400

    FEAT: Implemented backend

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+let todos = [
+  { id: 1, task: 'Buy groceries', completed: false },
+  { id: 2, task: 'Learn Node.js', completed: false },
+];
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(port, () => {
+  console.log(`App lisntening on port: ${port}`);
+});
