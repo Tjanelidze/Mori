ce5f9cfc9c492018fda110e41702dd95d0893fbf
t.janelidze <tedojanelidze7@gmail.com>
2026-02-20 16:58:14 +0400

    FEAT: implemented graceful shutdown

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 const port = process.env.PORT;
+let server;
 
 app.use(express.json());
 
@@ -15,10 +16,18 @@ app.use('/api/v1', routes);
 app.use(errorHandler);
 
 connectToDb().then(() => {
-    app.listen(port, () => {
+    server = app.listen(port, () => {
         console.log(`App listening on port: ${port}`);
     });
 }).catch((err) => {
     console.error('Failed to connect to database, shutting down.', err);
     process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    if (server) {
+        server.close(() => process.exit(1));
+    } else {
+        process.exit(1);
+    }
 });
